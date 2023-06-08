affca0f4067cf7afd509e7152f3f23aaa5a1d675
Syderi <syderi@yahoo.com>
2023-06-08 18:39:07 +0300

    fix: active Link

--- skilla/src/components/navbar/NavList.tsx
+++ skilla/src/components/navbar/NavList.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as LogoResults } from '../../assets/image/results.svg';
 import { ReactComponent as LogoOrders } from '../../assets/image/orders.svg';
 import { NavLink } from 'react-router-dom';
-import styles from './NavList.module.scss';
+import './NavList.scss';
 
 const navItems = [
   {
@@ -22,8 +22,8 @@ function NavList() {
   return (
     <>
       {navItems.map((nav) => (
-        <NavLink className={styles.navlink} to={nav.path} key={nav.id}>
-          <nav.logo fill={'white'} fill-opacity={0.56} alt={nav.name} />
+        <NavLink className="navlist" to={nav.path} key={nav.id}>
+          <nav.logo className="navlistLogo" fill={'white'} fillOpacity={0.56} alt={nav.name} />
           <div>{nav.name}</div>
         </NavLink>
       ))}
